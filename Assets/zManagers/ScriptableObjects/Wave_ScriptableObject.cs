1dc11039a9337e3d48577042a36cd9bb45881705
Matias Cabello <127139662+matthzc332@users.noreply.github.com>
2026-01-14 19:22:21 -0300

    cambiando metodo waves
    cambio forma que aparece las oleadas segun nivel, aun tiene fallos

--- Assets/zManagers/ScriptableObjects/Wave_ScriptableObject.cs
+++ Assets/zManagers/ScriptableObjects/Wave_ScriptableObject.cs
@@ -1,18 +1,53 @@
+//using UnityEngine;
+
+//[CreateAssetMenu(fileName = "Wave_ScriptableObject", menuName = "ScriptableObjects/Wave_ScriptableObject", order = 1)]
+//public class Wave_ScriptableObject : ScriptableObject
+//{
+//    [field: SerializeField]
+
+//    //numero de enemigos a generar
+//    public GameObject[] EnemiesInWave {  get; private set; }
+//    [field: SerializeField]
+
+//    // tiempo entre olas
+//    public float TimeBeforeThisWave { get; private set; }
+
+//    [field: SerializeField]
+//    //numero de spawn
+//    public float NumberToSpawn { get; private set; }
+//}
+
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Wave_ScriptableObject", menuName = "ScriptableObjects/Wave_ScriptableObject", order = 1)]
 public class Wave_ScriptableObject : ScriptableObject
 {
-    [field: SerializeField]
+    [Header("Configuración de la Oleada")]
 
-    //numero de enemigos a generar
-    public GameObject[] EnemiesInWave {  get; private set; }
-    [field: SerializeField]
-    
-    // tiempo entre olas
-    public float TimeBeforeThisWave { get; private set; }
+    [Tooltip("Arrastra aquí los prefabs de los enemigos (Gaviota, Globo, etc)")]
+    [SerializeField] private GameObject[] enemiesInWave;
 
-    [field: SerializeField]
-    //numero de spawn
-    public float NumberToSpawn { get; private set; }
+    [Tooltip("Tiempo de espera antes de empezar a spawnear")]
+    [SerializeField] private float timeBeforeThisWave;
+
+    [Tooltip("Cantidad total de enemigos a spawnear en esta tanda")]
+    [SerializeField] private float numberToSpawn;
+
+
+    // --- PROPIEDADES PÚBLICAS (Para que el Spawner pueda leerlas) ---
+
+    public GameObject[] EnemiesInWave
+    {
+        get { return enemiesInWave; }
+    }
+
+    public float TimeBeforeThisWave
+    {
+        get { return timeBeforeThisWave; }
+    }
+
+    public float NumberToSpawn
+    {
+        get { return numberToSpawn; }
+    }
 }
