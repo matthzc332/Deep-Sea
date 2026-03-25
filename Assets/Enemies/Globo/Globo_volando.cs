0d6250065ac76a7edf19edba07fbb260794a1a2c
Bruno Ojeda <brunofenix69@gmail.com>
2026-03-25 16:58:15 -0300

    Revert "Merge branch 'Develop-Matias-Cabello' into Testeo4"
    This reverts commit 1d47755ed5094ca2c0547066b23e0c4ef3659004, reversing
    changes made to 5f140ab2c781bfad183daf384f10d59211ec3459.

--- Assets/Enemies/Globo/Globo_volando.cs
+++ Assets/Enemies/Globo/Globo_volando.cs
@@ -21,47 +21,30 @@ public class Globo_Volando : State_Base
         if (barco == null || globo == null || anim == null)
             return;
 
-        Vector3 direccion = (barco.position - controlledObject.transform.position).normalized;
+        // Movimiento hacia el barco
+        Vector3 direccion =
+            (barco.position - controlledObject.transform.position).normalized;
 
-        // Usamos globo.speed directamente si es pública en Entity, o revisa getSpeed()
-        controlledObject.transform.Translate(direccion * globo.getSpeed() * Time.deltaTime, Space.World);
+        controlledObject.transform.Translate(
+            direccion * globo.getSpeed() * Time.deltaTime
+        );
 
-        anim.Play(0); // Animación Volando
+        // Animacion VOLANDO (indice 0)
+        anim.Play(0);
 
+        // Colision con el barco
         if (globo.getCollisionWithShip())
         {
             ExitState("Colision_con_barco");
+            return;
+        }
+
+        // Si muere
+        if (globo.getHP() <= 0)
+        {
+            ExitState("Globo_Explotando");
         }
     }
-    // public override void UpdateState()
-    // {
-    //     if (barco == null || globo == null || anim == null)
-    //         return;
-
-    //     // Movimiento hacia el barco
-    //     Vector3 direccion =
-    //         (barco.position - controlledObject.transform.position).normalized;
-
-    //     controlledObject.transform.Translate(
-    //         direccion * globo.getSpeed() * Time.deltaTime
-    //     );
-
-    //     // Animacion VOLANDO (indice 0)
-    //     anim.Play(0);
-
-    //     // Colision con el barco
-    //     if (globo.getCollisionWithShip())
-    //     {
-    //         ExitState("Colision_con_barco");
-    //         return;
-    //     }
-
-    //     // Si muere
-    //     if (globo.getHP() <= 0)
-    //     {
-    //         ExitState("Globo_Explotando");
-    //     }
-    // }
 
     public override void ExitState(string nextState)
     {
